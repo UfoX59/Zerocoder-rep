b525d05e4a00a47d69d3ce2109f71344453c393d
UfoX59 <alexd59@mail.ru>
2025-04-01 21:19:35 +0500

    Написал код домашнего задания

---
+++ PS02_hw_t1_1.py
@@ -0,0 +1,10 @@
+import requests
+
+# Задание 1: Получение данных
+params = {    'q' : 'html' }
+
+response = requests.get('https://api.github.com/search/repositories', params=params)
+response_json = response.json()
+
+print(response.status_code)
+print(f'количество репозиториев c использованием html: {response_json['total_count']}')
